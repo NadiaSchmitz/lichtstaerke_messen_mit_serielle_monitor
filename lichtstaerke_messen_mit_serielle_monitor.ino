83a8bf84a2875a2b4802350b52d3afe027099630
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-18 10:39:41 +0200

    First commit

---
+++ lichtstaerke_messen_mit_serielle_monitor.ino
@@ -0,0 +1,21 @@
+int entry = A0;
+int led_blue = 13;
+int led_blue_value;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led_blue, OUTPUT);
+}
+
+void loop() {
+  led_blue_value = analogRead(entry);
+  Serial.println("Sensorwert: ");
+  Serial.println(led_blue_value);
+    
+  if (led_blue_value < 512) {
+    digitalWrite(led_blue, HIGH);
+  } else {
+    digitalWrite(led_blue, LOW);
+  }
+  delay(1000);
+}
